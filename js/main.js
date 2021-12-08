7c4a18862c1dfab0d3444ce8f549d1f49a8cc9ab
Matheus silva <teus_26@hotmail.com>
2021-12-08 09:18:39 -0300

    js

--- js/main.js
+++ js/main.js
@@ -95,6 +95,6 @@ $(document).ready(function () {
   // Parallax
   setTimeout(function () {
     $('#data-area').parallax({ imageSrc: 'img/cidadeparallax.png' })
+    $('#apply-area').parallax({ imageSrc: 'img/pattern.png' })
   }, 250)
 })
-
